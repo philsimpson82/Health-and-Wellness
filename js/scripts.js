fa8708343a96440f6faeb204fe7cb575d4e45002
Phil Simpson <phil_simpson@att.net>
2022-10-12 18:27:03 -0500

    backup 10_12_22

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,8 @@ $(function () {
     $('#signupBtn').on('click', function () {
         $('#signupModal').modal('show');
     });
+    $('#bookingBtn').on('click', function () {
+        $('#bookingModal').modal('show');
+    });
 });
 
